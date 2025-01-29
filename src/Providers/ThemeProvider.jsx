b6f73f14fb0006f3c0d596b5b8cd1cc83d12996b
Mariam4324 <ashkmariam01@gmail.com>
2025-01-29 19:20:11 +0300

    feat: theme context added

---
+++ src/Providers/ThemeProvider.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext(null);
+
+export const ThemeProvider = ({ children }) => {
+    return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.jsx";
+import { ThemeProvider } from "./Providers/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router}>
-        <App />
-    </RouterProvider>
+    <ThemeProvider>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </ThemeProvider>
 );
